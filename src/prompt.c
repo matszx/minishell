9bc9dbbce4f2695f06b96150af2f64b73eb3f6ab
Mateusz Cygan <mcygan@mi-r8-p3.s19.be>
2024-09-04 12:36:20 +0200

    style: prompt colours

--- src/prompt.c
+++ src/prompt.c
@@ -6,11 +6,16 @@
 /*   By: mcygan <mcygan@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:14:02 by mcygan            #+#    #+#             */
-/*   Updated: 2024/09/03 14:51:23 by mcygan           ###   ########.fr       */
+/*   Updated: 2024/09/04 12:35:04 by mcygan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+#define	PT		"\x1b[38;5;195m"	// pale turquoise
+#define	AG		"\x1b[38;5;191m"	// apple green
+#define	RESET	"\x1b[0m"
+
 
 char	*prompt(void)
 {
@@ -18,7 +23,7 @@ char	*prompt(void)
 
 	while (1)
 	{
-		user_input = readline("minishell$ ");
+		user_input = readline(PT "minishell" AG " > " RESET);
 		if (!user_input)
 			eof_handler();
 		if (!ft_strncmp(user_input, "exit", 5))
