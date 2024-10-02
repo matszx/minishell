2eefdc578166187001530250f32b9b9b70f31e44
mcygan <coding@mcygan.anonaddy.me>
2024-10-02 16:25:44 +0200

    fix: bug in space_line

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: mcygan <mcygan@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:21:10 by mcygan            #+#    #+#             */
-/*   Updated: 2024/10/02 13:38:49 by mcygan           ###   ########.fr       */
+/*   Updated: 2024/10/02 16:25:01 by mcygan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +40,14 @@ char	*space_line(char *s)
 
 	spaces = 0;
 	i = 0;
-	while (s[i] && s[i + 1])
+	while (s[i])
 		spaces += sep_check(s, i++);
 	line = malloc(sizeof(char) * (ft_strlen(s) + spaces + 1));
 	if (!line)
 		return (printf("error: malloc fail, can't parse line\n"), NULL);
 	i = 0;
 	j = 0;
-	while (s[i] && s[i + 1])
+	while (s[i])
 	{
 		line[j++] = s[i];
 		if (sep_check(s, i++))
