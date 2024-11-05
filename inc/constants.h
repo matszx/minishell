43b095550c602ee09e371faf61d3c5b4e39f50af
matszx <coding@mcygan.anonaddy.me>
2024-11-05 13:43:35 +0100

    changed the green prompt colour to a greener one, IT'S NOT YELLOW

--- inc/constants.h
+++ inc/constants.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dzapata <dzapata@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mcygan <mcygan@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:52:15 by dzapata           #+#    #+#             */
-/*   Updated: 2024/10/26 17:18:54 by dzapata          ###   ########.fr       */
+/*   Updated: 2024/11/05 13:42:51 by mcygan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define CONSTANTS_H
 
 # define PT		"\x1b[1;38;5;195m"	// pale turquoise
-# define AG		"\x1b[1;38;5;191m"	// apple green
+# define AG		"\x1b[1;38;5;155m"	// apple green
 # define VM		"\x1b[1;38;5;203m"	// vermillion
 # define RESET	"\x1b[0m"			// reset
 
